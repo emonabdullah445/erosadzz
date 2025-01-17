73f03771bd1073342482e5e1d0b66041a222d2e2
Zayn Yusuf <yusue786@gmail.com>
2025-01-18 01:36:12 +0600

    not fix

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col-reverse gap-10  md:flex-row md:items-center md:justify-center">
-      <Navbar />
-      <LeftText />
-      <Login />
-      <Footer />
-    </div>
-    // <div className="">Eros</div>
+    // <div className="flex min-h-screen w-full flex-col-reverse gap-10  md:flex-row md:items-center md:justify-center">
+    //   <Navbar />
+    //   <LeftText />
+    //   <Login />
+    //   <Footer />
+    // </div>
+    <div className="">Eros</div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,19 @@ import LeftText from "../LeftText/LeftText";
 import Login from "../Login/Login";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import useMockLogin from "@/hooks/useMockLogin";
+import useMockLogin from "@/hooks/useMockLogin.js";
 
 const Home = ({ adminId, posterId }) => {
   const { login } = useMockLogin(adminId, posterId);
   return (
-    <div className="flex min-h-screen w-full flex-col-reverse gap-10  md:flex-row md:items-center md:justify-center">
+    <>
       <Navbar />
-      <LeftText />
-      <Login login={login} />
+      <div className="flex min-h-screen w-full flex-col-reverse gap-10  md:flex-row md:items-center md:justify-center">
+        <LeftText />
+        <Login login={login} />
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
