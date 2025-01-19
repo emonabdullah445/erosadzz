24637c36427604251988a3f397cc90df9112309a
Zayn Yusuf <yusue786@gmail.com>
2025-01-19 23:27:18 +0600

    fix password validation

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -4,10 +4,8 @@ const validationSchema = yup.object().shape({
     .string()
     .email("email must be a valid email")
     .required("Email is Required."),
-  password: yup
-    .string()
-    .required("Password is Required.")
-    .min(8, "Password must be at least 8 characters"),
+  password: yup.string().required("Password is Required."),
+  // .min(8, "Password must be at least 8 characters"),
   // .matches(
   //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
   //   "Password must contain at least 8 characters, including uppercase , lowercase and numbers"
